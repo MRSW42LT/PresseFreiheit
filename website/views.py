99c28f79b241bc69bc904337bfecf23d6256c094
Denis_Kehrwald_Jańhak <denisianhaki@outlook.com.br>
2022-11-19 23:29:50 -0300

    bfbfcbvcxbcv

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, url_for, redirect
 from flask_login import login_required, current_user
 from . import db
 from .models import Note, Article
